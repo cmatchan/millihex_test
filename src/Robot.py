1d4996dfc595f0d044b75eea4ab2466288d7b34e
cmatchan <cmchan@ucsd.edu>
2022-05-17 01:30:32 +0000

    Renamed joint.py to Robot.py and started outlining Robot class methods that will allow robot to move

--- src/Robot.py
+++ src/Robot.py
@@ -4,14 +4,15 @@ import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 
-class joint:
+
+class Robot:
     def __init__(self):
         """Initialize ROS publisher & ROS subscriber"""
         # Publish to joint position controller command topic
         self.joint_pub = rospy.Publisher("/millihex/leg1_joint1_position_controller/command", Float64, queue_size = 1)
 
         # Subscribe to robot joint_states topic
-        self.subscriber = rospy.Subscriber("/camera/image/compressed", JointState, self.callback)
+        self.subscriber = rospy.Subscriber("/millihex/joint_states", JointState, self.callback)
 
         # Store joint position
         self.position = Float64()
@@ -20,17 +21,25 @@ class joint:
         """Subscriber callback function of joint_states topic
         Here joint state positions are updated"""
         print("Joint Position: %s", ros_data.position)
+        rate = rospy.Rate(0.5)
+        rate.sleep()
 
-    def get_joint_position(self):
-        """Returns current joint position angle"""
+    def get_joint_positions(self):
+        """Returns current joint positions for robot"""
         return self.position
+
+    def compute_fk():
+        """Returns forward kinematics for legs"""
+
+    def compute_ik():
+        """Returns inverse kinematics for legs"""
 
 
 def main():
-    """Initializes ROS node for a single leg joint"""
+    """Initializes ROS node for robot"""
     try:
-        joint()
-        rospy.init_node('leg_joint', anonymous=True)
+        rospy.init_node('robot_definition', anonymous=True)
+        robot = Robot()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
