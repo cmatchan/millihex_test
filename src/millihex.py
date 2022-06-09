ac22f11baeb125875e46b796ac37bef7c971fe3a
cmatchan <cmchan@ucsd.edu>
2022-06-09 18:22:06 +0000

    Testing joint movement

--- src/millihex.py
+++ src/millihex.py
@@ -7,11 +7,21 @@ from sensor_msgs.msg import JointState
 class Robot:
     def __init__(self):
         """Initialize ROS publisher & ROS subscriber."""
+        num_legs = 6
+        joints_per_leg = 3
 
-        # Publish to /millihex/joint1_position1_controller/command topic
-        self.leg1_joint1_pub = rospy.Publisher( \
-            "/millihex/leg1_joint1_position_controller/command", \
-            Float64, queue_size = 1)
+        # Initialize joint publishers
+        # /millihex/leg#_joint#_position_controller/command
+        for i in range(num_legs):
+            for j in range(joints_per_leg):
+                leg_number = i+1
+                joint_number = j+1
+                publisher_name = f"self.leg{leg_number}_joint{joint_number}_pub"
+                publisher_topic = f"/millihex/leg{leg_number}" \
+                    f"_joint{joint_number}_position_controller/command"
+                start_publisher_command = f"{publisher_name} = " \
+                    f"rospy.Publisher('{publisher_topic}', Float64, queue_size=1, latch=True)"
+                exec(start_publisher_command)
 
         # Subscribe to millihex/joint_states topic
         self.subscriber = rospy.Subscriber("/millihex/joint_states", \
@@ -20,6 +30,7 @@ class Robot:
         # Keep track of all robot joint positions
         self.joint_positions = Float64()
 
+
     def callback(self, ros_data):
         """Subscriber callback function of /millihex/joint_states topic.
         Here, the joint state positions are updated every 2 seconds."""
@@ -27,13 +38,14 @@ class Robot:
         rate = rospy.Rate(0.5)                          # update every 2 sec
         rate.sleep()
 
+
     def get_joint_positions(self):
         """Returns current joint positions of robot"""
         return self.joint_positions
 
-    def publish_joint_positions(self, leg_number, joint_number, joint_position):
+
+    def publish_joint_position(self, leg_number, joint_number, joint_position):
         """Publishes new joint_position to specified leg_number and joint_number."""
-        
         publisher_name = f"leg{leg_number}_joint{joint_number}_pub"
         publish_command = f"self.{publisher_name}.publish({joint_position})"
         print(f"publish_command = {publish_command}")
--- src/walking_demo.py
+++ src/walking_demo.py
@@ -9,9 +9,27 @@ def main():
         rospy.init_node('robot_definition', anonymous=True)
         millihex = Robot()
         rate = rospy.Rate(0.5)
+        num_legs = 6
+        joints_per_leg = 3
+        
+        for i in range(num_legs):
+            for j in range(joints_per_leg):
+                leg_number = i+1
+                joint_number = j+1
+                millihex.publish_joint_position(leg_number, joint_number, 0)
 
         while not rospy.is_shutdown():
-            millihex.publish_joint_positions(1, 1, 1)
+            for i in range(int(num_legs/2)):
+                leg_number = i+1
+                millihex.publish_joint_position(leg_number, 3, 1)
+                millihex.publish_joint_position(int(leg_number+num_legs/2), 3, -1)
+            rate.sleep()
+            
+            
+            for i in range(int(num_legs/2)):
+                leg_number = i+1
+                millihex.publish_joint_position(leg_number, 3, 0)
+                millihex.publish_joint_position(int(leg_number+num_legs/2), 3, 0)
             rate.sleep()
 
     except rospy.ROSInterruptException:
