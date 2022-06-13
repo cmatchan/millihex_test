a19675c0a007c81efd09c4c7ca83cbaeb635c14d
cmatchan <cmchan@ucsd.edu>
2022-06-13 18:45:28 +0000

    Added check cases to make sure Publisher and Subscriber topics are connected to before publishing joint position commands

--- src/millihex.py
+++ src/millihex.py
@@ -6,12 +6,15 @@ from sensor_msgs.msg import JointState
 class Robot:
     def __init__(self, num_legs, joints_per_leg):
         """Initialize ROS publishers & ROS subscribers"""
+        self.num_legs = num_legs
+        self.joints_per_leg = joints_per_leg
+        self.num_joint_publishers = 0       # Number of joint publishers started
 
-        def start_all_joint_position_controller_publishers(self):
+        def start_joint_position_controller_publishers(self):
             """ Initialize joint publishers for the topic
             /millihex/leg#_joint#_position_controller/command """
-            for i in range(num_legs):
-                for j in range(joints_per_leg):
+            for i in range(self.num_legs):
+                for j in range(self.joints_per_leg):
                     leg_number = i+1        # Robot leg indexes from 1
                     joint_number = j+1      # Robot joint indexes from 1
 
@@ -26,10 +29,18 @@ class Robot:
                     start_publisher_command = f"self.{publisher_name} = " \
                         f"rospy.Publisher('{publisher_topic}', Float64, queue_size=1, latch=True)"
 
-                    exec(start_publisher_command)       # Execute string as a function
-        
+                    # Execute command string as a function
+                    exec(start_publisher_command)
+
+                    check_num_pub_connections = f"self.{publisher_name}.get_num_connections() < 1"
+                    while exec(check_num_pub_connections):
+                        rate = rospy.Rate(1)
+                        rate.sleep()
+
+                    self.num_joint_publishers = self.num_joint_publishers + 1
+
         # Initialize all joint position controllers for command topic
-        start_all_joint_position_controller_publishers(self)
+        start_joint_position_controller_publishers(self)
 
         # Subscribe to millihex/joint_states topic
         self.subscriber = rospy.Subscriber("/millihex/joint_states", \
@@ -43,11 +54,12 @@ class Robot:
         """Subscriber callback function of /millihex/joint_states topic.
         Here, the joint state positions are updated every 2 seconds."""
         self.joint_positions = ros_data.position        # Joint positions stored as tuple
-        rate = rospy.Rate(0.5)                          # Update every 2 sec
+        # print(f"Subscriber callback = {self.joint_positions}\n")
+        rate = rospy.Rate(1)
         rate.sleep()
 
 
-    def get_single_joint_position(self, leg_number, joint_number):
+    def get_joint_position(self, leg_number, joint_number):
         """Returns position of joint_number in leg_number"""
         joint_index = (leg_number - 1) * self.joints_per_leg + (joint_number - 1)
         return self.joint_positions[joint_index]
@@ -58,8 +70,8 @@ class Robot:
         return self.joint_positions
 
 
-    def publish_single_joint_position(self, leg_number, joint_number, joint_position):
-        """Publishes new joint_position to specified leg_number and joint_number."""
+    def set_joint_position(self, leg_number, joint_number, joint_position):
+        """Publishes new joint_position to specified leg_number, joint_number."""
         # Name of joint publisher = "leg#_joint#_pub"
         publisher_name = f"leg{leg_number}_joint{joint_number}_pub"
 
@@ -68,10 +80,8 @@ class Robot:
 
         exec(publish_command)       # Execute string as a function
 
-        # print(f"publish_command = {publish_command}")
 
-
-    def publish_all_joints_position(self, joint_position):
+    def set_all_joints_position(self, joint_position):
         # TODO
         pass
     
--- src/walking_demo.py
+++ src/walking_demo.py
@@ -6,21 +6,41 @@ from millihex import Robot
 def main():
     """Initializes ROS node for robot"""
     try:
-        rospy.init_node('robot_definition', anonymous=True)
-        rate = rospy.Rate(0.5)
+        rospy.init_node('robot_rock', anonymous=True)       # Initialize ROS node
+        rate = rospy.Rate(0.5)                              # Set refresh rate
 
+        # Initialize a Robot object to spawn millihex
         num_legs = 6
         joints_per_leg = 3
         millihex = Robot(num_legs, joints_per_leg)
 
-        # Set all 
+        print("ROBOT INITIALIZED")
+        print(f"Joint Position Publisher connections = {millihex.num_joint_publishers}\n")
+
+        print("Waiting for Joint States Subscriber...")
+        while millihex.subscriber.get_num_connections() < 1:
+            rate = rospy.Rate(1)
+            rate.sleep()
+
+        print(f"Joint States Subscriber connections = {millihex.subscriber.get_num_connections()}\n")
+
+        # Reset all joint positions to 0;
         for i in range(num_legs):
             for j in range(joints_per_leg):
-                leg_number = i+1
-                joint_number = j+1
-                millihex.publish_single_joint_position(leg_number, joint_number, 0)
+                leg_number = i+1                # Robot leg indexes from 1
+                joint_number = j+1              # Robot joint indexes from 1
 
-        # # Test damping parameters
+                # Set joint position = 0 to each joint in a leg
+                millihex.set_joint_position(leg_number, joint_number, 0)
+        
+        rate.sleep()
+    
+        # Test 
+        millihex.set_joint_position(1, 2, 1.3)
+        joint_position = millihex.get_joint_position(1,2)
+        print(f"leg1_link2 position = {joint_position}")
+
+        # # Testing loop
         # while not rospy.is_shutdown():
         #     for i in range(int(num_legs/2)):
         #         leg_number = i+1
